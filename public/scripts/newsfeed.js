b6df5394c8d38d644db791a3f67be3bd1e222ccf
Ryan Davis <ryancraigdavis@gmail.com>
2020-05-25 11:42:27 -0700

    Newsfeed is finished, changed curated newsfeed to a download of resources, still need to fix the CORS issue, need to add title explanation to the front page. Build out US dashboards.

--- public/scripts/newsfeed.js
+++ public/scripts/newsfeed.js
@@ -41,13 +41,40 @@ function getNews(){
     			// Data for the table - code similar to Week 6
     			var data = JSON.parse(req.responseText)
 
+    			// This loop goes through each of the articles and creates a table with the image, title, source, link,
+    			// and description appended to each row of the table.
     			for (var i = 0; i < 9; i++) {
+    				// Creates the rows and cells
 				  	var row = document.createElement("tr");
-			    	var cell = document.createElement("td");
-			    	var cellText = document.createTextNode("From "+data.articles[i].source.name+": "+data.articles[i].title);
-			    	cell.appendChild(cellText);
-			    	row.appendChild(cell);
-			    	cell.style.border = "thin solid black";
+			    	var cell1 = document.createElement("td");
+			    	var cell2 = document.createElement("td");
+			    	var cell3 = document.createElement("td");
+			    	var cell4 = document.createElement("td");
+
+			    	// These are the variables created from the API data
+			    	var articleTitle = document.createTextNode("From "+data.articles[i].source.name+": "+data.articles[i].title);
+			    	var articleDescription = document.createTextNode(data.articles[i].description);
+			    	var articleUrl = data.articles[i].url
+			    	var articleImageLink = data.articles[i].urlToImage
+
+			    	// These variables are for the url link and image
+			    	var articleImage = document.createElement('img');
+			    	articleImage.src = articleImageLink;
+			    	var articleLink = document.createElement('a');
+				    articleLink.appendChild('Click to Go');
+				    articleLink.title = 'Click to Go';
+				    articleLink.href = articleUrl;
+				    articleLink.target = '_blank'
+
+				    // Appending the variables to the cells
+				    cell1.appendChild(articleImage);
+			    	cell2.appendChild(articleTitle);
+			    	cell3.appendChild(articleDescription);
+			    	cell4.appendChild(articleDescription);
+			    	row.appendChild(cell1);
+			    	row.appendChild(cell2);
+			    	row.appendChild(cell3);
+			    	row.appendChild(cell4);
 				    // Appends rows to the table
 				  	tableBody.appendChild(row);
 				}
@@ -58,6 +85,8 @@ function getNews(){
 
 	    	} else {
 	      		console.error(req.statusText);
+	      		document.getElementById('newsfeed-bad-api').innerHTML += 
+				'<h3>Too Many API Requests</h3><p>Unfortunately the API has received too many requests, please wait a bit and try again</p>'
 	    	}
 	  	}
 	};
